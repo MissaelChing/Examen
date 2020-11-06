282429024acbb52e865db135c0ca68deaf3af6c3
MissaelChing <Missa_Ching@hotmail.com>
2020-11-06 08:25:21 -0600

    metodo en ingles cambiado

---
+++ Examen.Api/Controllers/CifrarController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using PokeAPI;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Examen.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CifrarController : ControllerBase
+    {
+        [HttpPost]
+        [Route("post")]
+        public async Task<IActionResult> PostAsync(string Text)
+        {
+            if (!string.IsNullOrEmpty(Text))
+            {
+                Random random = new Random();
+                int num = random.Next(1, 700);
+                PokemonSpecies species = await DataFetcher.GetApiObject<PokemonSpecies>(num);
+                StringBuilder Ms = new StringBuilder(Text);
+                EnC.CIFRAR(ref Ms, Text);
+                return Ok($"Escriba el Mensaje {Ms} \n" + $"Pokemon asignado como clave es: {species.Name} \n");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
---
+++ Examen.Api/EnC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.Api
+{
+    public class EnC
+    {
+        public static void CIFRAR(ref StringBuilder Mensaje, string Key)
+        {
+            string spanishAlphabet = "AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚÜVWXYZ";
+
+            for (int i = 0; i < Mensaje.Length; i++) Mensaje[i] = Char.ToUpper(Mensaje[i]);
+            Key = Key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < Mensaje.Length; i++)
+            {
+                if (spanishAlphabet.Contains(Mensaje[i]))
+                    Mensaje[i] = spanishAlphabet[(spanishAlphabet.IndexOf(Mensaje[i]) + spanishAlphabet.IndexOf(Key[j])) % spanishAlphabet.Length];
+                j = (j + 1) % Key.Length;
+            }
+        }
+        public static void CIFRARIn(ref StringBuilder Mensaje, string Key)
+        {
+            string englishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            for (int i = 0; i < Mensaje.Length; i++) Mensaje[i] = Char.ToUpper(Mensaje[i]);
+            Key = Key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < Mensaje.Length; i++)
+            {
+                if (englishAlphabet.Contains(Mensaje[i]))
+                    Mensaje[i] = englishAlphabet[(englishAlphabet.IndexOf(Mensaje[i]) + englishAlphabet.IndexOf(Key[j])) % englishAlphabet.Length];
+                j = (j + 1) % Key.Length;
+            }
+        }
+    }
+}
