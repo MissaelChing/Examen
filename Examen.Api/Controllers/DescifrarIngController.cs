45f81b08916af3a253f05ec7fefbfde62d9d5a8a
MissaelChing <Missa_Ching@hotmail.com>
2020-11-06 00:09:10 -0600

    Agregando carpeta que faltaba

---
+++ Examen.Api/Controllers/DescifrarIngController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Examen.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DescifrarIngController : ControllerBase
+    {
+        [HttpPost]
+        [Route("post")]
+        public IActionResult Post([FromHeader] string TMensaje, [FromHeader] string clave)
+        {
+            if (!string.IsNullOrEmpty(TMensaje) && !string.IsNullOrEmpty(clave))
+            {
+                StringBuilder Mensaje = new StringBuilder(TMensaje);
+                DesenC.DESCIFRAR(ref Mensaje, clave);
+                return Ok(Mensaje.ToString());
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
